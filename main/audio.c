dbef69983fcfcb113d1655d79c6987a9286553f7
Scot Bontrager <scot@indievisible.org>
2026-02-13 20:03:14 -0600

    clean up some biz

--- main/audio.c
+++ main/audio.c
@@ -15,20 +15,18 @@
 
 #include "sd.h"
 
-static i2s_chan_handle_t                tx_chan;        // I2S tx channel handler
-static i2s_chan_handle_t                rx_chan;        // I2S rx channel handler
+static i2s_chan_handle_t tx_chan;
 
 static void i2s_write_task(void *args) {
     int16_t *w_buf = (int16_t *)calloc(AUDIO_BUFFSIZE, sizeof(int16_t));
     assert(w_buf);
-    size_t w_bytes = AUDIO_BUFFSIZE;
 
     /* Enable the TX channel */
     ESP_ERROR_CHECK(i2s_channel_enable(tx_chan));
 
     while (1) {
 	synthTick(w_buf, AUDIO_BUFFSIZE);
-        if (i2s_channel_write(tx_chan, w_buf, AUDIO_BUFFSIZE, &w_bytes, 1000) != ESP_OK) {
+        if (i2s_channel_write(tx_chan, w_buf, AUDIO_BUFFSIZE, NULL, 1000) != ESP_OK) {
             printf("Write Task: i2s write failed\n");
             break;
         }
@@ -38,20 +36,18 @@ static void i2s_write_task(void *args) {
 }
 
 void start_audio(void) {
-    // look into i2s_channel_register_event_callback()
-
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
-    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, &tx_chan, &rx_chan));
+    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, &tx_chan, NULL));
 
     i2s_std_config_t std_cfg = {
         .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(SAMPLE_RATE),
         .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
         .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,    // some codecs may require mclk signal, this example doesn't need it
+            .mclk = I2S_GPIO_UNUSED,
             .bclk = I2S_BCLK_IO,
             .ws   = I2S_WS_IO,
             .dout = I2S_DOUT_IO,
-            .din  = I2S_DOUT_IO, // duplex
+            .din  = I2S_GPIO_UNUSED,
             .invert_flags = {
                 .mclk_inv = false,
                 .bclk_inv = false,
@@ -60,15 +56,11 @@ void start_audio(void) {
         },
     };
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_chan, &std_cfg));
-    // ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_chan, &std_cfg));
 
     xTaskCreate(i2s_write_task, "i2s_write_task", 4096, NULL, 5, NULL);
 }
 
 void stop_audio(void) {
-  i2s_channel_disable(tx_chan);
-  // i2s_channel_disable(rx_chan);
-
-  i2s_del_channel(tx_chan);
-  i2s_del_channel(rx_chan);
+  ESP_ERROR_CHECK(i2s_channel_disable(tx_chan));
+  ESP_ERROR_CHECK(i2s_del_channel(tx_chan));
 }
--- main/leaf.c
+++ main/leaf.c
@@ -6,12 +6,20 @@
 
 #include "sd.h"
 
+// ideal situation: 2 osc - square & saw + sub, mixable.
+// amp and filter env
+// filter with reso, because aciiiieeeeeed
+
 typedef struct {
-    // tCycle *osc;
-    tSawtooth *osc;
+    tCycle *subosc;
+    Lfloat submix;
+    tSawtooth *saw;
+    Lfloat sawmix;
+    tSquare *square;
+    Lfloat squaremix;
     // tTwoPole *filter;
-    // tEnvelope *ampEnv; // env for amp
-    // tEnvelope *filtEnv; // AD env for filter freq
+    tEnvelope *ampEnv; // env for amp
+    // tEnvelope *filtEnv; // env for filter freq
 } synth_t;
 
 static LEAF leaf;
@@ -28,19 +36,31 @@ void stop_leaf(void) {
 void start_leaf(void) {
     LEAF_init(&leaf, SAMPLE_RATE, leafMempool, LEAF_MEM_POOL_SIZE, &rando);
 
-    // tCycle_init(&synth.osc, &leaf);
-    // tCycle_setFreq(synth.osc, 440.0);
-    tSawtooth_init(&synth.osc, &leaf);
-    tSawtooth_setSampleRate(synth.osc, SAMPLE_RATE);
+    tCycle_init(&synth.subosc, &leaf);
+    tCycle_setSampleRate(synth.subosc, SAMPLE_RATE);
+    synth.submix = 0.25;
 
-    assert(&synth.osc);
+    tSawtooth_init(&synth.saw, &leaf);
+    tSawtooth_setSampleRate(synth.saw, SAMPLE_RATE);
+    synth.sawmix = 0.5;
+
+    tSquare_init(&synth.square, &leaf);
+    tSquare_setSampleRate(synth.square, SAMPLE_RATE);
+    synth.squaremix = 0.5;
+
+    assert(&synth.subosc);
+    assert(&synth.saw);
+    assert(&synth.square);
+
+    tCycle_setFreq(synth.subosc, 0.0);
+    tSawtooth_setFreq(synth.saw, 0.0);
+    tSquare_setFreq(synth.square, 0.0);
 
     // tTwoPole_init(&synth.filter, &leaf);
     // assert(&synth.filter);
 
-    // tEnvelope_init(&synth.ampEnv, 0.05f, 0.9999f, 0, &leaf);
-    // tEnvelope_on(synth.ampEnv, 60);
-    // assert(&synth.ampEnv);
+    tEnvelope_init(&synth.ampEnv, 0.05f, 0.9999f, 0, &leaf);
+    assert(&synth.ampEnv);
 
     // tEnvelope_init(&synth.filtEnv, 0.05f, 0.9999f, 0, &leaf);
     // tEnvelope_on(synth.filtEnv, 60);
@@ -53,17 +73,18 @@ void noteOn(uint8_t midipitch, uint8_t velocity) {
     // if note_is_on, noteOff()...
 
     freq = LEAF_midiToFrequency(midipitch);
-    // tCycle_setFreq(synth.osc, freq);
-    tSawtooth_setFreq(synth.osc, freq);
+    tCycle_setFreq(synth.subosc, freq / 2);
+    tSawtooth_setFreq(synth.saw, freq);
+    tSquare_setFreq(synth.square, freq);
 
-    // tEnvelope_on(synth.ampEnv, velocity);
+    tEnvelope_on(synth.ampEnv, velocity);
     // tEnvelope_on(synth.filtEnv, velocity);
-    // tCycle_setFreq(synth.osc, 0.0);
 }
 
 void noteOff(uint8_t midipitch) {
-    // tCycle_setFreq(synth.osc, 0.0);
-    tSawtooth_setFreq(synth.osc, 0.0);
+    tCycle_setFreq(synth.subosc, 0.0);
+    tSawtooth_setFreq(synth.saw, 0.0);
+    tSquare_setFreq(synth.square, 0.0);
 
     // stop amp env -- no need with the simple A/D tEnvelope
     // stop filter env -- no need
@@ -73,11 +94,17 @@ void synthTick(int16_t *buf, int bufsiz) {
    Lfloat samp = 0.00f;
 
    for (int i = 0; i < bufsiz; i++) {
-       // samp = tCycle_tick(synth.osc);
-       samp = tSawtooth_tick(synth.osc);
-       // samp *= tEnvelope_tick(synth.ampEnv);
+      samp = (synth.squaremix * tSquare_tick(synth.square)) + 
+               (synth.sawmix * tSawtooth_tick(synth.saw)) +
+               (synth.submix * tCycle_tick(synth.subosc));
+      samp *= (0.9f * tEnvelope_tick(synth.ampEnv));
+
+//     tSVF_setFreq(cowbell->bandpassOsc, cowbell->filterCutoff + 1000.0f * tEnvelope_tick(cowbell->envFilter));
+//     sample = tSVF_tick(cowbell->bandpassOsc,sample);
+
        // tTwoPole_setB0(synth.filter, tEnvelope_tick(synth.filtEnv));
        // samp = tTwoPole_tick(synth.filter, samp);
-       buf[i] = (int16_t)(samp * 32767.0); // convert to 16-bit integer
+
+      buf[i] = (int16_t)(samp * 32765.0); // convert to 16-bit integer
    }
 }
